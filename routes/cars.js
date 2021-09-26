0d50201a8efa27cc10bf46f0aba96d5d64e4bd75
Michael Lan <74420477+michaellanuk@users.noreply.github.com>
2021-09-26 10:27:40 +0100

    Find all cars and renamed table

--- routes/cars.js
+++ routes/cars.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const db = require('../config/database')
+const Car = require('../db/models/car')
 
-router.get('/', (req, res) => res.send('Cars'))
+router.get('/', (req, res) => 
+    Car.findAll()
+        .then(cars => {
+            console.log(cars)
+            res.sendStatus(200)
+        })
+        .catch(err => console.log(err))
+)
 
 module.exports = router
