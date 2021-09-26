093fc3a995c72528ccd6842762f89e2184fec258
Michael Lan <74420477+michaellanuk@users.noreply.github.com>
2021-09-26 10:46:55 +0100

    Add car

--- routes/cars.js
+++ routes/cars.js
@@ -3,13 +3,38 @@ const router = express.Router()
 const db = require('../config/database')
 const Car = require('../db/models/car')
 
-router.get('/', (req, res) => 
+router.get('/', (req, res) => {
     Car.findAll()
         .then(cars => {
-            console.log(cars)
-            res.sendStatus(200)
+            res.send(cars)
         })
         .catch(err => console.log(err))
+    }
 )
+
+router.post('/add', (req, res) => {
+    let { make, model } = req.body
+    let errors = []
+
+    if (!make) {
+        errors.push({text: "Please enter a make"})
+    }
+    if (!model) {
+        errors.push({text: "Please enter a model"})
+    }
+
+    if (errors.length > 0) {
+        res.send(errors)
+    }
+
+    Car.create({
+        make,
+        model,
+        createdAt: new Date(),
+        updatedAt: new Date()
+    })
+        .then(car => res.redirect('/cars'))
+        .catch(err => console.log(err))
+})
 
 module.exports = router
